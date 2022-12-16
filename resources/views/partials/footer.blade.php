d8d22f37109e856e539a40bdbabdc312dc3ac853
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-12-16 16:11:27 +0100

    Initial Commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,118 @@
+<footer>
+
+    <div id="footer" class="d-flex justify-content-center">
+        <div class="section d-flex p-5">
+            <div class="links">
+                <ul> DC COMICS
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                </ul>
+                <ul> DC COMICS
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="links">
+                <ul> DC COMICS
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="links">
+                <ul> DC COMICS
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                    <li>
+                        <a href="#">link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="dcBackground">
+            <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+    </div>
+
+        <div id="socials">
+            <div class="callToAction d-flex justify-content-between align-items-center">
+                <div class="signUpNowWrapper p-4">
+                    <button class="signUpNow">Sign Up Now!</button>
+                </div>
+                <div class="socialsWrapper d-flex justify-content-between p-4">
+                    <a href="#">
+                        <h3>Follow Us</h3>
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../assets/img/footer-facebook.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,43 @@
+<header>
+        <nav class="d-flex justify-content-center">
+            
+            <div class="section d-flex justify-content-between">
+                <div class="imgWrapper p-4">
+                    <img src="../assets/img/dc-logo.png" alt="">
+                </div>
+
+                <div class="navLinksWrapper d-flex align-items-center">
+                    <div class="linkWrapper">
+                        <a href="#">Characters</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Comics</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Movies</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Characters</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Comics</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Movies</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Characters</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Comics</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Movies</a>
+                    </div>
+                    <div class="linkWrapper">
+                        <a href="#">Characters</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
